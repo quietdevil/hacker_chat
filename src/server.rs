de5817a51bbe55afbe4b31b832be90f88355965c
gin <tander.secret@gmail.com>
2026-05-08 12:26:27 +0300

    new architecture

--- src/server.rs
+++ src/server.rs
@@ -9,10 +9,10 @@ use std::{
     thread,
 };
 
-enum Event {
-    Join(Message),
+enum ServerEvent {
+    NewClient { id: u32, stream: TcpStream },
     Message(Message),
-    Quit(Message),
+    Disconnect { client_id: u32 },
 }
 
 #[derive(Debug, Clone)]
@@ -21,142 +21,123 @@ struct Message {
     msg: String,
 }
 
-struct Connect {
+struct Client {
     id: u32,
-    send_channel: Sender<Event>,
-}
-
-impl Connect {
-    fn build(id: u32, send_channel: Sender<Event>) -> Connect {
-        Connect {
-            id: id,
-            send_channel: send_channel,
-        }
-    }
-
-    fn new(self, stream: TcpStream) -> Sender<Message> {
-        let (sender, receiver) = channel::<Message>();
-        let stream_reader = stream.try_clone().unwrap();
-        let id = self.id;
-
-        let greeating = format!("Hacker with id {id} join in server\n");
-
-        self.send_channel
-            .send(Event::Join(Message { id, msg: greeating }))
-            .unwrap();
-
-        thread::spawn(move || {
-            loop {
-                let mut msg = String::new();
-                let mut buf = BufReader::new(&stream_reader);
-
-                // if close connection loop break
-                if let Ok(end) = buf.read_line(&mut msg) {
-                    // EOF
-                    if end == 0 {
-                        let msg = pretty_message("Connect closed");
-                        self.send_channel
-                            .send(Event::Quit(Message { id, msg }))
-                            .unwrap();
-
-                        break;
-                    }
-                };
-
-                let message = pretty_message(&msg);
-
-                match self.send_channel.send(Event::Message(Message {
-                    id: id,
-                    msg: message,
-                })) {
-                    Ok(()) => println!("receive message id {}", id),
-                    Err(err) => eprintln!("{err}"),
-                }
-            }
-        });
-
-        let mut stream_writter = stream.try_clone().unwrap();
-
-        thread::spawn(move || {
-            while let Ok(val) = receiver.recv() {
-                if id == val.id {
-                    continue;
-                }
-
-                if let Err(err) = write!(&mut stream_writter, "{}", val.msg) {
-                    println!("{}", err);
-                    break;
-                }
-            }
-        });
-        sender
-    }
+    tx: Sender<Message>,
 }
 
 pub struct Server {
-    connections: Arc<Mutex<Vec<Sender<Message>>>>,
+    connections: Vec<Client>,
+    tx: Sender<ServerEvent>,
+    rx: Receiver<ServerEvent>,
 }
 
 impl Server {
     pub fn build() -> Server {
-        let vec_cons = Arc::new(Mutex::new(Vec::new()));
+        let (tx, rx) = channel();
+        let vec_cons = Vec::new();
         Server {
             connections: vec_cons,
+            tx,
+            rx,
         }
     }
 
-    pub fn run(self, ipaddr: Ipv4Addr, port: u16) {
-        let (sender_client, receiver_server) = channel();
-
+    pub fn run(&mut self, ipaddr: Ipv4Addr, port: u16) {
         let addr = SocketAddr::new(std::net::IpAddr::V4(ipaddr), port);
         let listener = TcpListener::bind(addr).expect("Trouble with port");
 
-        let vec_conns = Arc::clone(&self.connections);
+        let tx = self.tx.clone();
 
         thread::spawn(move || {
             let mut id = 0;
             for stream in listener.incoming() {
                 match stream {
                     Ok(stream) => {
-                        let send = sender_client.clone();
-                        let client = Connect::build(id, send);
-                        id += 1;
-                        Self::add_connect(vec_conns.clone(), stream, client);
+                        tx.send(ServerEvent::NewClient { id: id, stream }).unwrap();
                     }
                     Err(err) => panic!("{}", err),
                 }
+                id += 1;
             }
         });
 
+        self.event_loop();
+    }
+
+    fn event_loop(&mut self) {
         loop {
-            match receiver_server.recv() {
+            match self.rx.recv() {
                 Ok(event) => match event {
-                    Event::Join(msg) => self.send_all(msg),
-                    Event::Message(msg) => self.send_all(msg),
-                    Event::Quit(msg) => self.send_all(msg),
+                    ServerEvent::NewClient { id, stream } => {
+                        let tx = self.tx.clone();
+                        let client_tx = Self::spawn_client(id, stream, tx);
+                        self.connections.push(Client { id, tx: client_tx });
+                    }
+                    ServerEvent::Disconnect { client_id } => self.send_all(Message {
+                        id: client_id,
+                        msg: pretty_message("Hacker leave chat"),
+                    }),
+                    ServerEvent::Message(msg) => self.send_all(msg),
                 },
-                Err(err) => println!("{}", err),
+                Err(err) => println!("{err}"),
             }
         }
     }
 
-    fn add_connect(
-        connections: Arc<Mutex<Vec<Sender<Message>>>>,
-        stream: TcpStream,
-        client: Connect,
-    ) {
-        connections.lock().unwrap().push(client.new(stream));
-    }
+    fn spawn_client(id: u32, stream: TcpStream, server_tx: Sender<ServerEvent>) -> Sender<Message> {
+        let (tx, rx) = channel::<Message>();
 
-    fn send_all(&self, message: Message) {
-        let mut share_msg = self.connections.lock().unwrap();
-        share_msg.retain(|tx| match tx.send(message.clone()) {
-            Ok(_) => true,
-            Err(err) => {
-                println!("{}", err);
-                false
+        let stream_reader = stream.try_clone().unwrap();
+        let mut stream_writer = stream.try_clone().unwrap();
+
+        let server_tx = server_tx.clone();
+
+        thread::spawn(move || {
+            let mut buf = BufReader::new(&stream_reader);
+
+            loop {
+                let mut msg = String::new();
+
+                match buf.read_line(&mut msg) {
+                    Ok(0) => server_tx
+                        .send(ServerEvent::Disconnect { client_id: id })
+                        .unwrap(),
+                    Ok(_) => server_tx
+                        .send(ServerEvent::Message(Message {
+                            id,
+                            msg: pretty_message(&msg),
+                        }))
+                        .unwrap(),
+                    Err(_) => server_tx
+                        .send(ServerEvent::Disconnect { client_id: id })
+                        .unwrap(),
+                };
             }
         });
+
+        thread::spawn(move || {
+            while let Ok(val) = rx.recv() {
+                if id == val.id {
+                    continue;
+                }
+
+                if let Err(err) = write!(&mut stream_writer, "{}", val.msg) {
+                    println!("{}", err);
+                    break;
+                }
+            }
+        });
+
+        tx
+    }
+
+    fn send_all(&mut self, msg: Message) {
+        self.connections
+            .retain(|client| match client.tx.send(msg.clone()) {
+                Ok(_) => true,
+                Err(_) => false,
+            });
     }
 }
 
